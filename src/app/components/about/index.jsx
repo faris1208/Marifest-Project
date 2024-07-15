2c43bcd0b81a4fd79fe3b22d58f971a7f95cbe76
faris1208 <farislawal3@gmail.com>
2024-07-15 12:44:46 -0700

    new commit

--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -51,7 +51,7 @@ export default function About() {
           <div className={styles.story_text}>
             <b>TEAM MARITEST</b>
             <h2>Our Story</h2>
-            <p>In 2021, an unexpected encounter changed our founder Kakooza Hakim's life that sparked the creation of MariTest. While interning on the remote Bussi Island in Uganda, Kakooza experienced a severe bout of fever. Despite having faced fever before, this time was different and much worse.</p>
+            <p>In 2021, an unexpected encounter changed our founder Kakooza Hakims life that sparked the creation of MariTest. While interning on the remote Bussi Island in Uganda, Kakooza experienced a severe bout of fever. Despite having faced fever before, this time was different and much worse.</p>
             <p>One morning, Kakooza felt the familiar signs of malaria. But by this time, the fever had escalated. He went to a local healthcare center, only to wait three grueling hours and be told that they had run out of Malaria Rapid Diagnostic Test kits (RDTs) and antimalarial drugs. With COVID-19 lockdowns in place, he was stuck on the island with few options.</p>
             <p>Desperate, Kakooza turned to local remedies to manage his fever. His condition worsened, but luckily, a special travel permit allowed him to reach a private hospital on the mainland. By then, the parasites had severely damaged his liver, bringing him close to death.</p>
           </div>
@@ -60,7 +60,7 @@ export default function About() {
           <div className={styles.story_wrapper_two_text}>
             <p>During his recovery, Kakooza reflected on the harsh reality faced by those without access to adequate healthcare. Malaria is a massive burden in Africa, infecting 241 million people and killing over half a million in 2020 alone. Every 75 seconds, a child under five died from the disease. The major issues were limited access to early diagnosis and treatment, poor vector control, lack of medical facilities, and a shortage of healthcare workers.</p>
             <p>Determined to make a difference, Kakooza, leveraging his machine learning expertise from Fundi Bots Uganda and entrepreneurial skills from the African Leadership University, teamed up with four other amazing individuals with diverse skills but on a shared mission to fight malaria in sub-Saharan Africa. MariTest was created.</p>
-            <p>MariTest is a reusable, non-invasive, automated diagnostic and prescription device that was designed to bridge the gap in malaria diagnosis and treatment, embodying the team's commitment to a malaria-free future.</p>
+            <p>MariTest is a reusable, non-invasive, automated diagnostic and prescription device that was designed to bridge the gap in malaria diagnosis and treatment, embodying the teams commitment to a malaria-free future.</p>
           </div>
           <div className={styles.yellow_box}>
             <Image
